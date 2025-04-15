73f1c501d7ddac3503125e51fe1a1229c48b83b7
cuiyue <cuiyue.msn@hotmail.com>
2025-04-15 15:21:28 +0800

    修bug，不必再判断旧predict.json格式

--- src/model/default/ai-switch/effects.ts
+++ src/model/default/ai-switch/effects.ts
@@ -22,7 +22,8 @@ export default {
     *readAiConfig({ payload }: AnyAction, { call, put }: EffectsCommandMap) {
         const { casePath } = payload as { casePath: string };
         try {
-            const temp: PredictComp = yield call([helper, 'readJSONFile'], tempAt);
+            const temp: PredictJson = yield call([helper, 'readJSONFile'], tempAt);
+
             if (casePath === undefined) {
                 //无案件目录，是新增，读模版
                 yield put({ type: 'setData', payload: (temp as { config: Predict[] }).config });
@@ -33,40 +34,11 @@ export default {
                 const exist: boolean = yield call([helper, 'existFile'], aiConfigAt);
                 if (exist) {
                     //案件下存在，读取案件下的predict.json
-                    const caseAi: PredictComp = yield call([helper, 'readJSONFile'], aiConfigAt);
-
-                    if (Array.isArray(caseAi)) {
-                        const ret = (caseAi as Predict[]).reduce((total: Predict[], current: Predict) => {
-                            const next = caseAi.find((i) => i.type === current.type);
-                            if (next !== undefined) {
-                                total.push({ ...current, use: next.use });
-                            } else {
-                                total.push(current);
-                            }
-                            return total;
-                        }, []);
-                        yield put({ type: 'setData', payload: ret });
-                        yield put({ type: 'setSimilarity', payload: (temp as PredictJson).similarity });
-                        yield put({ type: 'setOcr', payload: (temp as PredictJson).ocr });
-                    } else {
-                        const ret: PredictJson = { config: [], similarity: caseAi.similarity, ocr: caseAi.ocr };
-                        ret.config = (temp as PredictJson)
-                            .config
-                            .reduce((total: Predict[], current: Predict) => {
-                                const next = (caseAi as PredictJson)
-                                    .config
-                                    .find((i) => i.type === current.type);
-                                if (next !== undefined) {
-                                    total.push({ ...current, use: next.use });
-                                } else {
-                                    total.push(current);
-                                }
-                                return total;
-                            }, []);
-                        yield put({ type: 'setData', payload: ret.config });
-                        yield put({ type: 'setSimilarity', payload: ret.similarity });
-                        yield put({ type: 'setOcr', payload: ret.ocr });
-                    }
+                    const caseAi: PredictJson = yield call([helper, 'readJSONFile'], aiConfigAt);
+                    const next = { ...temp, ...caseAi };
+                    yield put({ type: 'setData', payload: next.config });
+                    yield put({ type: 'setSimilarity', payload: next.similarity });
+                    yield put({ type: 'setOcr', payload: next.ocr });
                 } else {
                     //不存在，读取模版
                     const next: PredictComp = yield call([helper, 'readJSONFile'], tempAt);
